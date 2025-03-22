4e63b72d25484278cb50dccf2ed605f70d868b49
M4v3r1cX <m4v3r1cx.thanatos@gmail.com>
2025-03-21 21:14:09 -0300

    inicio del engine de audio

---
+++ src/com/bsodsoftware/abraxas/engine/sound/Audio.java
@@ -0,0 +1,25 @@
+package com.bsodsoftware.abraxas.engine.sound;
+
+public class Audio {
+
+    private String location;
+    // Parece que no se va a poder hacer con esta arquitectura, asique intentemos este
+    // https://www.geeksforgeeks.org/play-audio-file-using-java/
+
+    public Audio(String location) {
+        this.location = location;
+        this.load();
+    }
+
+    private void load() {
+        try {
+            String location = this.getClass().getResource(this.location);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void play() {
+
+    }
+}
